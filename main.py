92c466ab5832602ab5cf5b073cc0ac5aa3b4fc24
manishch7 <choudhary.man@northeastern.edu>
2025-03-18 15:37:08 -0400

    Added Enriched and DataLoading File in main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
 from twitter_client import authenticate, scrape_tweets
 from utils import log_error  # Added for error handling
+from Enriched_Tweets import process_tweets
+from Data_Loading_Neo4j import load_tweets_data_into_neo4j
 
 async def main():
     """Main entry point"""
@@ -8,6 +10,8 @@ async def main():
         client = await authenticate()
         if client:
             await scrape_tweets(client)
+            process_tweets()
+            load_tweets_data_into_neo4j()
     except Exception as e:
         log_error("main", e)
 
